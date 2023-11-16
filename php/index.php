65bbe362bbf249f6e5d1ede25bcae356419a0d06
yooksj13 <yooksj13@ewhain.net>
2023-11-17 01:39:34 +0900

    update index.php

--- php/index.php
+++ php/index.php
@@ -3,16 +3,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
+    <title>Big Rolling</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            padding: 15px;
+            text-align: center;
+            color: #fff; /* 흰색으로 변경 */
+        }
+
+        nav {
+            display: flex;
+            justify-content: center;
+            background-color: #555;
+            padding: 10px;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            padding: 10px 20px;
+            margin: 0 10px;
+            border-radius: 5px;
+            background-color: #777;
+            transition: background-color 0.3s;
+        }
+
+        nav a:hover {
+            background-color: #999;
+        }
+    </style>
 </head>
 <body>
 
-    <h1>Main Page</h1>
+    <header>
+        <h1>Big Rolling</h1>
+    </header>
     
-    <a href="search_product.php"><button>search_product</button></a>
-    <a href="user_ranking.php"><button>user_ranking</button></a>
-    <a href="order_list.php"><button>order_list</button></a>
-    <a href="inventory_management.php"><button>inventory_management</button></a>
+    <nav>
+        <a href="search_product.php">search_product</a>
+        <a href="user_ranking.php">user_ranking</a>
+        <a href="order_list.php">order_list</a>
+        <a href="inventory_management.php">inventory_management</a>
+    </nav>
 
 </body>
 </html>
